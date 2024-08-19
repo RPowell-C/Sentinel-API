b8407ab6ccd2262cef62b20f15b6b4519011f88f
Reid Powell <garitbibbs@gmail.com>
2024-08-19 13:43:08 -0500

    testing on prod because i wanna

--- api/recieve.ts
+++ api/recieve.ts
@@ -1,3 +1,24 @@
+import table from './../table.json';
+
+// this entire interface is useless but I don't want to delete because I spent so much effort on it
+interface HashTable {
+    filename: string;
+    SentinelPy: string;
+    packageJSON: string;
+    packageLockJSON: string;
+    README: string;
+    requirementsTxt: string;
+    functions: string;
+    ApiPy: string;
+    InternalFunctions: string;
+    moderationPy: string;
+    UCALPy: string;
+    filesayPy: string;
+    internalPy: string;
+    settingsPy: string;
+}
+
+
 export default function handler(req, res) {
-    res.status(200).json({message: 'sup', hashes: 'this feature is being worked on' });
+    res.status(200).json({message: 'sup', table });
 }
