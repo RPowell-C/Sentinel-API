1d08c6f7ba12c2cab9d5f8b0c1b6c69cb063dd02
Reid Powell <garitbibbs@gmail.com>
2024-08-19 12:20:05 -0500

    added hash table and another API response

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
     res.status(200).json({message: 'sup', version: '0.1.0' });
 }
+
---
+++ api/recieve.ts
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.status(200).json({message: 'sup', hashes: 'this feature is being worked on' });
+}
